b8ceb8dcc0009343318bd2f2141becfa4ef4eac8
Makan Azarshahy <oro1919@gmail.com>
2018-04-19 15:59:48 -0700

    Added working sumLists

--- 04-19-18/sumLists.js
+++ 04-19-18/sumLists.js
@@ -17,3 +17,73 @@ Output: 9 -> 1 -> 2. That is 912.
 
 Hints #7, #30, #71, #95, #109
 */
+
+// i: two nodes that have reverse numbers
+// o: a new node that is added from the previous nodes
+// c: must return the numbers in reversed order
+// e: empty list
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+};
+
+class LinkedList {
+  constructor(val) {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToTail(node) {
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+}
+
+const calculateValAndOver = (current1, current2, carryOver) => {
+  const val1 = current1 ? current1.val : 0;
+  const val2 = current2 ? current2.val : 0;
+  const newVal = val1 + val2 + carryOver;
+  return {
+    value: newVal >= 10 ? newVal % 10 : newVal,
+    over: newVal >= 10 ? 1 : 0,
+  };
+};
+
+const sumLists = (node1, node2) => {
+  let carryOver = 0;
+  let result = new LinkedList();
+  let current1 = node1;
+  let current2 = node2;
+
+  while(current1 || current2) {
+    const { value, over } = calculateValAndOver(current1, current2, carryOver);
+    const node = new Node(value);
+    carryOver = over;
+    
+    result.addToTail(node);
+
+    current1 = current1 ? current1.next : null;
+    current2 = current2 ? current2.next : null;
+  }
+
+  if (carryOver) {
+    result.addToTail(new Node(carryOver));
+  }
+
+  return result;
+};
+
+module.exports = {
+  Node,
+  LinkedList,
+  sumLists,
+  calculateValAndOver,
+};
